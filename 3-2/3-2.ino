c353cdb97ae2fef6b3af210fac64cf8987ce9457
job.ljee555@gmail.com <job.ljee555@gmail.com>
2020-03-25 12:16:40 +0700

    2success

--- 3-2/3-2.ino
+++ 3-2/3-2.ino
@@ -1,5 +1,5 @@
 void setup(){
-pinMode(2, OUTPUT);
+    pinMode(2, OUTPUT);
     pinMode(3, OUTPUT);
     pinMode(4, OUTPUT);
     pinMode(5, OUTPUT);
@@ -14,5 +14,32 @@ pinMode(2, OUTPUT);
 }
 
 void loop(){
-
+    digitalWrite(2, 1);
+    digitalWrite(3, 1);
+    digitalWrite(4, 1);
+    digitalWrite(5, 1);
+    digitalWrite(6, 1);
+    digitalWrite(7, 1);
+    delay(500);
+    digitalWrite(2, 0);
+    digitalWrite(3, 0);
+    digitalWrite(4, 0);
+    digitalWrite(5, 0);
+    digitalWrite(6, 0);
+    digitalWrite(7, 0);
+    delay(75);
+    digitalWrite(8, 1);
+    digitalWrite(9, 1);
+    digitalWrite(10, 1);
+    digitalWrite(11, 1);
+    digitalWrite(12, 1);
+    digitalWrite(13, 1);
+    delay(500);
+    digitalWrite(8, 0);
+    digitalWrite(9, 0);
+    digitalWrite(10, 0);
+    digitalWrite(11, 0);
+    digitalWrite(12, 0);
+    digitalWrite(13, 0);
+    delay(75);
 }
