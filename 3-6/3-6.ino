4bf51d07fd97f6943634354e8e36e8d98fe9fe42
job.ljee555@gmail.com <job.ljee555@gmail.com>
2020-03-25 22:19:24 +0700

    test6

--- 3-6/3-6.ino
+++ 3-6/3-6.ino
@@ -1,6 +1,39 @@
 void setup(){
-    
+    Serial.begin(9600);
+    pinMode(3,INPUT_PULLUP);
 }
+int z = 0;
+int y = 0;
+int one = 1;
+int two = 2;
+int three = 3;
 void loop(){
+    int x = 0;
+    while (digitalRead(3)==0) {
+    x++;
+    delay(50);
+    if (x == 1){
+        y++;
+        
+        Serial.println(y);
+        if (one + z == y){
+            z++;
+            Serial.println(one);
+        }
+        
+        else if (two + z == y){
+            z++;
+            Serial.println(two)
+        }
+        else if (three + z == y){
+            z++;
+            Serial.println(three);
+        }
 
+    }
+    }
+
+    
+
+    
 }
