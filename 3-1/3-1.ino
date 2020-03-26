82875e03a7e024e12b541f2c6fdd1339b3ed33cf
job.ljee555@gmail.com <job.ljee555@gmail.com>
2020-03-27 00:06:59 +0700

    all

--- 3-1/3-1.ino
+++ 3-1/3-1.ino
@@ -1,5 +1,17 @@
 void setup(){
-
+    
+    pinMode(2, OUTPUT);
+    pinMode(4, OUTPUT);
+    pinMode(5, OUTPUT);
+    pinMode(6, OUTPUT);
+    pinMode(7, OUTPUT);
+    pinMode(8, OUTPUT);
+    pinMode(9, OUTPUT);
+    pinMode(10, OUTPUT);
+    pinMode(11, OUTPUT);
+    pinMode(12, OUTPUT);
+    pinMode(13, OUTPUT);
+    pinMode(14, OUTPUT);
 }
 void loop(){
     digitalWrite(2, 1);
@@ -29,4 +41,8 @@ void loop(){
     digitalWrite(12, 0);
     digitalWrite(13, 0);
     delay(500);
+
+
+
+    
 }   
