458005a94d789629908f96b374a158ee8a7a078b
job.ljee555@gmail.com <job.ljee555@gmail.com>
2020-03-26 15:45:28 +0700

    edit 3-2

--- 3-2/3-2.ino
+++ 3-2/3-2.ino
@@ -1,16 +1,5 @@
 void setup(){
-    pinMode(2, OUTPUT);
-    pinMode(3, OUTPUT);
-    pinMode(4, OUTPUT);
-    pinMode(5, OUTPUT);
-    pinMode(6, OUTPUT);
-    pinMode(7, OUTPUT);
-    pinMode(8, OUTPUT);
-    pinMode(9, OUTPUT);
-    pinMode(10, OUTPUT);
-    pinMode(11, OUTPUT);
-    pinMode(12, OUTPUT);
-    pinMode(13, OUTPUT);
+   
 }
 
 void loop(){
